48d7ba68e891ee4241a62b03d85fb253a0ff5981
Charles <charles.jacquin@autistici.org>
2020-02-10 14:26:16 +0100

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "88ec55c8980f95b9c5f5";
+/******/ 	var hotCurrentHash = "2ab6f56753d2989955fb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
